cfde08ff0c01e52999ce81856585d2307e4b5730
VeloTeam-App <abhishek.bajpai@velocitai.com>
2022-11-06 17:13:08 +0530

    4:43:00

---
+++ 15_hashTables.py
@@ -0,0 +1,16 @@
+class hashTable:
+    def __init__(self,key,value) -> None:
+        self.key=key
+        self.value=value
+    
+    def insert(self, key,value):
+        pass
+    
+    def find(self, key):
+        pass
+    
+    def update(self, key, value):
+        pass
+    
+    def list_all(self):
+        pass
