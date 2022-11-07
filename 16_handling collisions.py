47ba18f09ede948e73d0b25798b1300e2016406a
VeloTeam-App <abhishek.bajpai@velocitai.com>
2022-11-07 18:35:22 +0530

    Linear probing of hash function(5:08:36)
    5:08:36

--- 16_handling collisions.py
+++ 16_handling collisions.py
@@ -1,9 +1,6 @@
 MAX_HASH_TABLE_SIZE=4096
 data_list=[None]*4096
 
-for item in data_list:
-    assert item==None
-
 #this function just ad the unicode of all characters in a string take the remainder of it by length of data list and return it
 def get_index(data_list, a_string):
     #variable to store the result (updated after each iteration)
@@ -21,23 +18,23 @@ def get_index(data_list, a_string):
 #print(get_index(data_list,'Abhishek')) 
 #Thus now we can store the item at the get index position of the data List
 
-key,value='Aakash','9797979997'
-idx=get_index(data_list,key)
-data_list[idx]=(key, value)
+# key,value='Aakash','9797979997'
+# idx=get_index(data_list,key)
+# data_list[idx]=(key, value)
 
 #this whole thing can be written in the single line of code as follows
 
-data_list[get_index(data_list,'Hemanth')]=('Hemanth','95959595454')
+# data_list[get_index(data_list,'Hemanth')]=('Hemanth','95959595454')
 
 #to find the value associated with the key in this we can simply get th ehasgh value
-f=get_index(data_list,'Aakash')
-key,value=data_list[f]
-print(key,value)
+# f=get_index(data_list,'Aakash')
+# key,value=data_list[f]
+# print(key,value)
 
 # To print all the valus of the this hash map we can simply do this
 
-keys=[kv[0]   for kv in data_list if kv is not None]
-print(keys)
+# keys=[kv[0]for kv in data_list if kv is not None]
+# print(keys)
 
 
 
@@ -46,7 +43,7 @@ class hashTable:
         self.data_list=[None]*max_size
     
     def insert(self, key,value):
-        idx=get_index(self.data_list,key)
+        idx = get_index(self.data_list,key)
         self.data_list[idx]=(key,value)
     
     def find(self, key):
@@ -66,23 +63,51 @@ class hashTable:
         return [kv[0] for kv in data_list if kv is not None]
     
 basic_table=hashTable(max_size=1024)
-# print(len(basic_table.data_list))
 
-# Inserting into the hash map named basic table 
-basic_table.insert('Aakash','9999999999')
-basic_table.insert('Hemant', '888888888')
+basic_table.insert('listen',99)
+basic_table.insert('silent',200)
+# print(basic_table.find('listen'))
 
-# Finding a value in the hash map
-print(basic_table.find('Hemant'))
 
-# Updating a value
-
-basic_table.update('Aakash', '777777777')
-
-print(basic_table.find('JAYSURYA'))
+def get_valid_index(data_list,key):
+    idx=get_index(data_list,key)
+    while True:
+        k=data_list[idx]
+        if k is None:
+            return idx
+        
+        if k==key:
+            return idx
+        idx+=1
+        if idx==len(data_list):
+            idx=0
     
-# get the list of all keys
-# print(basic_table.list_all())
+data_list2=[None]*MAX_HASH_TABLE_SIZE
+print(get_valid_index(data_list2,'listen')==655)
+
+data_list2[get_index(data_list2,'listen')]=('listen',99)
+print(get_valid_index(data_list2,'silent')==656)
+            
+            
+
+
+class ProbingHashTable:
+    def __init__(self, max_size=MAX_HASH_TABLE_SIZE) -> None:
+        self.data_list=[None]*max_size
     
+    def insert(self, key, value):
+        idx= get_valid_index(self.data_list,key)
+        self.data_list[idx]=key,value
+        
+    def find(self, key):
+        idx=get_valid_index(self.data_list, key)
+        kv=self.data_list[idx]
+        return None if kv is None else kv[1]
     
-   
+    def update(self,key, value):
+        idx=get_valid_index(self.data_list,key)
+        self.data_list[idx]= (key,value)
+        
+    def list_all(self):
+        return [kv[0] for kv in self.data_list if kv is not None]
+        
