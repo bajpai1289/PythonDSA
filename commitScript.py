a762ec0d7899f5a8880bd00bc169afa15b1b17fe
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-01-13 09:39:40 +0530

    Did questions

---
+++ commitScript.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+import os
+
+
+msg = "Did questions" 
+repo_directory = os.getcwd()
+
+subprocess.run(["git", "add", "."], cwd=repo_directory)
+# commit file
+subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
+# push
+subprocess.run(["git", "push"], cwd=repo_directory)  
