ca973e779c6bca05d68b011575a782c54ea639e6
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-01-20 18:15:24 +0530

    Did questions

--- commitScript.py
+++ commitScript.py
@@ -10,4 +10,4 @@ subprocess.run(["git", "add", "."], cwd=repo_directory)
 # commit file
 subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
 # push
-subprocess.run(["git", "push"], cwd=repo_directory)  
+subprocess.run(["git", "push"], cwd=repo_directory)     
