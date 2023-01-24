24d6431d24320300c90c60e47e1bef03b9f91f7d
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-01-24 18:39:51 +0530

    did

---
+++ 36_greedyBulbs.py
@@ -0,0 +1,15 @@
+def bulbs(A: list[int]):
+    cost = 0
+    for b in A:
+        if cost%2==0:
+            b=b
+        else:
+            b=not b
+        
+        if b%2==1:
+            continue
+        else:
+            cost+=1
+    return cost
+
+print(bulbs([1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]))
