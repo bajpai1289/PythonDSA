08275ab99807cadd006ea9b1d37fb04161869daa
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-01-30 09:34:00 +0530

    Did questions

--- practice question/test.py
+++ practice question/test.py
@@ -1,3 +1,9 @@
+def isEven(n):
+      if n%2==0:
+            return False
+      else:
+             return True
 arr=[str(i) for i in range(100,1000) if i%5==0]
-for i in arr:
-    if all(int(c)%2==0 for c in i)
+
+arr=[i for i in arr if not any(isEven( int(c)) for c in i)]
+print(arr)
