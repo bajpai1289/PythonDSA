c045ce10fa1bf4f23fb933d0587de438e82ba726
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-05 21:13:37 +0530

    completed backtracking

--- Advance Algorithms/BackTracking/backTrack.py
+++ Advance Algorithms/BackTracking/backTrack.py
@@ -10,3 +10,15 @@ def canReachLeaf(root):
     if canReachLeaf(root.right):
         return True
     return False
+
+def tracePath(root , path:'list' = []):
+    if not root or root.val ==0:
+        return False
+    path.append(root.val)
+    if root.left is None and root.right is None:
+        return True
+    if tracePath(root.left, path):
+        return True
+    if tracePath(root.right, path):
+        return True
+    return False
