4e395a3877fddc431fb4f35193fbfe4a629d0183
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-09 18:15:24 +0530

    Did questions

--- practice question/90_searcingInMatrix.py
+++ practice question/90_searcingInMatrix.py
@@ -5,14 +5,27 @@ def searchMatrix(matrix: 'list[list[int]]', target: int) -> bool:
         while lo<=hi:
             mid=(lo+hi)//2
             if arr[mid]==target:
-                return True
+                return mid
             elif arr[mid]<target:
                 lo=mid+1
             elif arr[mid]>target:
                 hi=mid-1
         return False
+    lo=0
+    hi = len(matrix)-1
+    while lo<=hi:
+        mid=(lo+hi)//2
+        if matrix[mid][0]<=target and matrix[mid][-1]>=target:
+            return search(matrix[mid], target=target), mid
+        elif matrix[mid][0]>target:
+            hi=mid-1
+        elif matrix[mid][-1]<target:
+            lo=mid+1
+    return False
     
 
 
 print(searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3))
-print(searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13))
+print(searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 34))
+print(searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 32))
+print(searchMatrix(matrix = [[1]], target = 1))
--- practice question/91_test.py
+++ practice question/91_test.py
@@ -1,3 +1,13 @@
 def search(arr: 'list[int]', target: int):
-    return target
-print(search([1,3,4,5,6,7,8]))
+    lo=0
+    hi=len(arr)-1
+    while lo<=hi:
+        mid=(lo+hi)//2
+        if arr[mid]==target:
+            return True
+        elif arr[mid]<target:
+            lo=mid+1
+        elif arr[mid]>target:
+            hi=mid-1
+    return False
+print(search([1,3,4,5,6,7,8],9))
