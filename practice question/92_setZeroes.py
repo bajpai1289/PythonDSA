1714250abcc517738dd5ec4cabf40db4bcf47312
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-15 18:15:24 +0530

    Did questions

---
+++ practice question/92_setZeroes.py
@@ -0,0 +1,25 @@
+matrix=[[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+def setZeroes(matrix: list[list[int]]) -> None:
+    indexes=set()
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):          
+            if matrix[r][c]==0:
+                indexes.add((r,c))
+    rows=[]
+    cols=[]
+    for row, col in indexes:
+        rows.append(row)
+        cols.append(col)
+    r,c=0,0
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):  
+            if (r, c) in indexes:
+                matrix[r][c]=0
+            if c in cols:
+                matrix[r][c]=0
+
+
+
+
+setZeroes(matrix)
+print(matrix==[[0,0,0,0],[0,4,5,0],[0,3,1,0]])
---
+++ practice question/93_removeLLelements.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, val) -> None:
+        self.val=val
+        self.next=None
+
+class LinkedList:
+    def __init__(self, defualtList=[]) -> None:
+        self.head=None
+
+    def append(self, val):
+        if self.head is None:
+            self.head=Node(val)
+        else:
+            curr=self.head
+            while curr.next is not None:
+                curr=curr.next
+            curr.next=Node(val)
+    
+    def show(self):
+        curr= self.head
+        while curr:
+            print(curr.val, end="=>")
+            curr=curr.next
+
+    def removeElements(self, val: int):
+        
+            prev=None
+            curr=self.head
+            while curr:
+                if curr.val==val:
+                    if prev:
+                        prev.next = curr.next
+                    else:
+                        self.head=curr.next
+                    curr=curr.next
+                else:
+                    prev=curr
+                    curr=curr.next
+
+        #removal at the last:
+        
+
+        
+
+
+list1=LinkedList()
+# list1.show()
+list1.append(6)
+list1.append(1)
+list1.append(2)
+list1.append(6)
+list1.append(6)
+list1.append(6)
+list1.append(3)
+list1.append(4)
+list1.append(5)
+list1.append(6)
+list1.show()
+print()
+list1.removeElements(6)
+list1.show()
