38aac3a644daa0b4ebc26d2c4c6da8b695143d70
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-23 18:15:24 +0530

    Did questions

--- practice question/98_isPlaindrome.py
+++ practice question/98_isPlaindrome.py
@@ -1,6 +1,6 @@
 # def isPalindrome(strs: str):
 #     return [i.lower() for i in strs if i.isalnum()]==[i.lower() for i in strs if i.isalnum()][::-1]
-isPalindrome = lambda strs: [i.lower() for i in strs if i.isalnum()]==[i.lower() for i in strs if i.isalnum()][::-1]
+def isPalindrome(strs): return [i.lower() for i in strs if i.isalnum()]==[i.lower() for i in strs if i.isalnum()][::-1]
 
 print(isPalindrome('A man, a plan, a canal. Panama'))
 print(isPalindrome('My age is 0, 0 si ega ym.'))
@@ -8,4 +8,3 @@ print(isPalindrome('1 eye for of 1 eye.'))
 print(isPalindrome('0_0 (: /-\ :) 0-0'))
 print(isPalindrome('eye'))
 print(isPalindrome('_eye'))
-print(isPalindrome)
---
+++ practice question/99_implementingMap.py
@@ -0,0 +1,17 @@
+class List:
+    def __init__(self, arr) -> None:
+        self.arr=arr
+
+    def myMap(self, callback):
+        newArr=[]
+        for i in self.arr:
+            newArr.append(callback(i))
+        return newArr
+    
+
+l=List([2,4,6,8,10])
+a=l.myMap(lambda x:x**2)
+print(a)
+
+a=list(map(lambda a: a**2, [2,4,6],[1,3,5]))
+print(a)
