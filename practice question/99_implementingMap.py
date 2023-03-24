efc973e98d79f2a11666e70857129736a34895e2
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-24 12:53:17 +0530

    Did questions

--- practice question/99_implementingMap.py
+++ practice question/99_implementingMap.py
@@ -2,6 +2,7 @@ class List:
     def __init__(self, arr) -> None:
         self.arr=arr
 
+
     def myMap(self, callback):
         newArr=[]
         for i in self.arr:
@@ -13,5 +14,9 @@ l=List([2,4,6,8,10])
 a=l.myMap(lambda x:x**2)
 print(a)
 
-a=list(map(lambda a: a**2, [2,4,6],[1,3,5]))
+a=[*map(lambda a: a**2,( [2,4,6,1,3,5]))]
+
+b=list(2,3,4,5)
+print(b)
+
 print(a)
