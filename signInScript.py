86a994b8341b2216c89442b70e7e82381535a39a
Abhishek Bajpai <58601525+bajpai1289@users.noreply.github.com>
2023-06-07 10:22:31 +0530

    Update signInScript to check the date and time

--- signInScript.py
+++ signInScript.py
@@ -2,6 +2,18 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 from passwords import VelocitaiPassword, VelocitaiUsername
+import datetime
+
+def is_weekend_morning():
+    current_time = datetime.datetime.now().time()
+    current_day = datetime.datetime.now().weekday()
+
+    is_weekend = current_day == 5 or current_day == 6
+    is_morning = datetime.time(8, 0) <= current_time <= datetime.time(11, 0)
+
+    return is_weekend or not is_morning
+if is_weekend_morning():
+    exit(0)
 
 browser = webdriver.Chrome()
 browser.get("https://velocitai.greythr.com/uas/portal/auth/login")
